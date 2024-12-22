d155a1f3ced531fe19cba1e9d33ec9091b9547b0
Tanisha <tanisha.engineer2005@gmail.com>
2024-12-22 23:20:10 +0530

    Used aggregation pipelines to fetch user channel details

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -5,12 +5,10 @@ const subscriptionSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User"
     },
-    subscribers: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-    ]
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 }, {timestamps: true});
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
